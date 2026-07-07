eb6630ddc1c81786ed86ece8cb0ee91858fc5c03
iqbal22pcr <m.iqbal22ti@mahasiswa.pcr.ac.id>
2026-07-07 18:19:01 +0700

    feat: add user roles with validation guard

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,50 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use RuntimeException;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * The roles allowed for this application, matching the MySQL enum
+     * values defined on the `role` column.
+     *
+     * @var list<string>
+     */
+    public const ROLES = [
+        'admin',
+        'kepala_sekolah',
+        'guru',
+        'staf_keuangan',
+        'staf_ppdb',
+        'wali_murid',
+    ];
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::saving(function (User $user) {
+            if (! in_array($user->role, self::ROLES, true)) {
+                throw new RuntimeException(
+                    "Invalid role \"{$user->role}\" assigned to User. Allowed roles: ".implode(', ', self::ROLES)
+                );
+            }
+        });
+    }
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array<string, mixed>
+     */
+    protected $attributes = [
+        'role' => 'wali_murid',
+    ];
 
     /**
      * The attributes that are mass assignable.
